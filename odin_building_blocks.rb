74b35cdabefe24e6752d361f519029c1f0f95854
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-10 16:07:40 +0100

    finished

--- odin_building_blocks.rb
+++ odin_building_blocks.rb
@@ -4,6 +4,14 @@ def cesears_cipher(input, step)
   	val = letter.ord
   	if val.between?(65,90) || val.between?(97,122)
   	  val += step
+  	  	case val
+  	  	  when 91 then val = 65
+  	  	  when 92 then val = 66
+  	  	  when 93 then val = 67
+  	  	  when 123 then val = 97
+  	  	  when 124 then val = 98
+  	  	  when 125 then val = 99
+  	  	end
   	  letter = val.chr
   	else letter = letter
   	end
@@ -12,4 +20,3 @@ end
 
 cesears_cipher("In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques.", 3)
 
-"In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques."
