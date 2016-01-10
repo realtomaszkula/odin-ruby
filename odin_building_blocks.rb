a96ebc737c8042eeb26dc16fa8ccd909dd0ce8c7
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-10 16:18:47 +0100

    working cesears_cipher

--- odin_building_blocks.rb
+++ odin_building_blocks.rb
@@ -1,21 +1,23 @@
 def cesears_cipher(input, step)
   text = input.split(//)
+
   text.collect! do |letter|
   	val = letter.ord
-  	if val.between?(65,90) || val.between?(97,122)
+  	overflow = 0
+  	if val.between?(65,90)
   	  val += step
-  	  	case val
-  	  	  when 91 then val = 65
-  	  	  when 92 then val = 66
-  	  	  when 93 then val = 67
-  	  	  when 123 then val = 97
-  	  	  when 124 then val = 98
-  	  	  when 125 then val = 99
-  	  	end
+  	  	overflow = val - 90 if val > 90
+  	    val = 65 + overflow
+  	  letter = val.chr
+  	elsif val.between?(97,122)
+  	  val += step
+  	    overflow = val - 122 if val > 122
+  	    val = 97 + overflow
   	  letter = val.chr
   	else letter = letter
   	end
   end.join("")
+
 end
 
 cesears_cipher("In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques.", 3)
