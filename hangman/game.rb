54da8601fb960b559458ac6bb87664d6d4de8393
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-19 21:08:46 +0100

    Class layout

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,32 +1,47 @@
-file = File.open("dictionary.txt",'r') do |fname|
-	file = fname.read.split(/\n/)
-end
+class Hangman
+	def initialize(name)
+		@name = name
+		@secret_word = get_secret
+		@letters = @secret_word.split("")
+		@bool_pairs = get_pairs
+	end
 
-secret_word = file.sample
-letters = secret_word.split("")
-bool_letters = []
+	def play
+		input = get_input
+		input.size > 1 ? check_word(input) : check_letter(input)
+	end
 
-letters.each { |letter| bool_letters << [letter,false] }
+	def get_secret
+		file = File.open("dictionary.txt",'r') do |fname|
+			file = fname.read.split(/\n/)
+		end
+		secret_word = file.sample
+	end	
 
-p bool_letters
-
-
-puts "type a letter or try to guess a word"
-input = gets.chomp
-
-if input.size > 1
-	check_word(input)
-else
-	check_letter(input)
-end
-
-def check_word(word)
-	 secret_word == word
-end
-
-def check_letter(letter)
-end
+	def get_pairs
+		@bool_pairs = []
+		@letters.each { |letter| @bool_pairs << [letter,false] }
+	end
 	
+	def get_input
+		puts "type a letter or try to guess a word"
+		input = gets.chomp
+	end
+	
+	def check_word(word)
+		 secret_word == word
+	end
+	
+	def check_letter(letter)
+		bool_letters.each do |bool_pair|
+			@bool_pair[1] = true if @bool_pair[0] == letter
+		end
+	end
+end
+
+puts "Enter name to play HangMaaaan"
+name = gets.chomp
+Hangman.new(name)
 
 
 
